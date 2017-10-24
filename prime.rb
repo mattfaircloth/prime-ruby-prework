30fec17577c946afde215ef57d2afc3b137a66a6
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-24 01:51:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  count_start = 2
+
+    if number == 0 or number == 1 or number < 0
+      return false
+    end
+
+    while count_start < number
+      if number % count_start != 0
+        count_start += 1
+      else
+        return false
+    end
+  end
+    return true
+end
